7ecb663bd9061c09fc52dfed57e2fcdf7e3db143
edwinpav <edwinpavlovsky@gmail.com>
2024-04-02 18:04:19 -0400

    update producer

--- Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
+++ Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
@@ -1,46 +1,42 @@
 import pika
 import os
 
-from producer.producer_interface import mqProducerInterface
+from producer_interface import mqProducerInterface
 
 class mqProducer(mqProducerInterface):
-    routing_key = None
-    exchange_name = None
-    connection = None
-    channel = None
 
     def __init__(self, routing_key: str, exchange_name: str) -> None:
         # Save parameters to class variables
-        routing_key = routing_key
-        exchange_name = exchange_name
+        self.routing_key = routing_key
+        self.exchange_name = exchange_name
+        self.connection = None
+        self.channel = None
 
         # Call setupRMQConnection
-        mqProducer.setupRMQConnection(self)
+        self.setupRMQConnection()
         
 
     def setupRMQConnection(self) -> None:
         # Set-up Connection to RabbitMQ service
         con_params = pika.URLParameters(os.environ["AMQP_URL"])
-        connection = pika.BlockingConnection(parameters=con_params)
+        self.connection = pika.BlockingConnection(parameters=con_params)
 
         # Establish Channel
-        mqProducer.channel = connection.channel()
+        self.channel = self.connection.channel()
 
         # Create the exchange if not already present
-        mqProducer.exchange = mqProducer.channel.exchange_declare(exchange=mqProducer.exchange_name)
-
-        mqProducer.publishOrder("test message")
+        exchange = self.channel.exchange_declare(exchange=self.exchange_name)
 
     def publishOrder(self, message: str) -> None:
         # Basic Publish to Exchange
-        mqProducer.channel.basic_publish(
-            exchange=mqProducer.exchange,
-            routing_key=mqProducer.routing_key,
+        self.channel.basic_publish(
+            exchange=self.exchange_name,
+            routing_key=self.routing_key,
             body=message,
         )
 
         # Close Channel
-        mqProducer.channel.close()
+        self.channel.close()
 
         # Close Connection
-        mqProducer.connection.close()
+        self.connection.close()
