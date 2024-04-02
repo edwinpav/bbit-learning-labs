dd2708569ed4d838c895fc0f4b04cfc85cfb096a
edwinpav <edwinpavlovsky@gmail.com>
2024-04-02 18:32:19 -0400

    make consumer for topic-exchange

--- Tech-Lab-On-Campus/Topic-Exchange/consume.py
+++ Tech-Lab-On-Campus/Topic-Exchange/consume.py
@@ -23,7 +23,7 @@ def main(sector: str, queueName: str) -> None:
     #
     #                       WRITE CODE HERE!!!
     #
-    
+    bindingKey = sector
     consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)    
     consumer.startConsuming()
     
@@ -35,5 +35,6 @@ if __name__ == "__main__":
     #
     #                       WRITE CODE HERE!!!
     #
-
+    sector = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
+    queue = sys.argv[2] if len(sys.argv) > 2 else 'anonymous.info'
     sys.exit(main(sector,queue))
